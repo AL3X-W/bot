d05630c9fcf66da6a1ea12dd32777eff7414e6ed
Alex W <39382374+AL3X-W@users.noreply.github.com>
2018-08-08 21:24:23 +0200

    Create mainbot.py

---
+++ mainbot.py
@@ -0,0 +1,36 @@
+import asyncio
+
+import discord
+
+import random
+
+import time
+
+
+#:lsupa4lf?
+
+from discord.ext import commands
+
+
+
+
+bot = commands.Bot(command_prefix='-')
+
+
+whitelist = ['-cheaters', '-invites', '-invite', '-top']
+
+@bot.listen()
+async def on_message(message):
+    role = discord.utils.get(message.guild.roles, name='Member')
+    if message.author == bot.user:
+        return
+    if role not in message.author.roles:
+        return
+    contents = message.content.split()
+    if not any(word in whitelist for word in contents):
+        await message.delete()
+
+
+
+
+bot.run("NDc2NzAyNzk2NjkyMDYyMjA4.DkxoRQ.J-UEFiAQkiaizRZ1MBnYhXVm_To")
